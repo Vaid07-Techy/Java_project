60e8590e4612f3105eee38e50cca37dafde77113
Vaidehi <vaidehigawde5@gnail.com>
2026-09-07 16:13:21 +0530

    Java Project upload

---
+++ src/test/java/com/example/aws_java_app/AwsJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws_java_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
